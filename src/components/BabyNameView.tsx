89d8c259e440e0c758adac1f9a39e0fe6de06b16
Maria <mtens17@gmail.com>
2022-11-17 13:13:55 +0000

    Add some styling

--- src/components/BabyNameView.tsx
+++ src/components/BabyNameView.tsx
@@ -1,19 +1,16 @@
-import { useState } from "react";
 import { OneBaby } from "../babyNamesData";
 import "../styles.css";
 
-type HandleFavs = (event: any) => void;
-
 interface BabyNamesProps {
   baby: OneBaby;
-  onClick: HandleFavs;
+  onClick: (event: any) => void;
 }
 export const favs: string[] = [];
 
 export function BabyName(props: BabyNamesProps): JSX.Element {
   return (
     <button
-      className={props.baby.sex === "f" ? "femaleb" : "maleb"}
+      className= {props.baby.sex === "f" ? "femaleb" : "maleb"}
       key={props.baby.id}
       //include an onClick function that has the name saved in a fav list
       onClick={props.onClick}
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,12 @@
-
 interface TextProps {
   value: string;
   onChange: (typedName: string) => void;
 }
 
-export function SearchBar({ value, onChange }: TextProps): JSX.Element {
+export function SearchBar(props: TextProps): JSX.Element {
   return (
-    <>
-      <input placeholder="Search a name" value={value} onChange={(event) => onChange(event.target.value)} />
-    </>
+    <div className = "margin-top">
+      <input  className= "searchbar" placeholder="Search a name" value={props.value} onChange={(event) => props.onChange(event.target.value)} />
+    </div>
   );
 }
